6e54f79583ddd09cc2a60e8ad5555a2d872e5c82
Ton Nguyen <ton.nguyen@episerver.com>
2014-11-05 13:17:47 +0700

    removed login dialog title, fixed some tab indent, and the swipe problem
    removed login dialog title, fixed some tab indent, and the swipe problem

--- www/js/xhrWrapper.js
+++ www/js/xhrWrapper.js
@@ -11,34 +11,34 @@ define([
                   _ajaxHandler: _ajaxHandler,
                   ajax: function (url, options) {
                         var deferred = $.Deferred();
-                  var urlArgument = url;
-                  var optionArgument = options;
-                  var xhrResult = this._ajaxHandler(urlArgument, optionArgument);
+                        var urlArgument = url;
+                        var optionArgument = options;
+                        var xhrResult = this._ajaxHandler(urlArgument, optionArgument);
 
-                  $.when(xhrResult).then(function(data) {
-                        // succeed
-                        deferred.resolve(data);
-                  }, function (jqXHR, error, textStatus) {
-                        // fail
-                        if (jqXHR.status == "401") {
-                              // need to login
-                              $.when(loginAction()).then(function (accessToken) {
-                                    // login successful, let's retry
-                                    var authorizationHeader = { "Authorization": "Bearer " + accessToken };
-                                    if (optionArgument) {
-                                          optionArgument.headers = authorizationHeader;
-                                    } else {
-                                          urlArgument.headers = authorizationHeader;
-                                    }
-                                    $.when($._ajaxHandler(urlArgument, optionArgument)).then(deferred.resolve, deferred.reject);
-                              }, function (err) {
-                                    // login failed, pass the error back to the caller
+                        $.when(xhrResult).then(function(data) {
+                              // succeed
+                              deferred.resolve(data);
+                        }, function (jqXHR, error, textStatus) {
+                              // fail
+                              if (jqXHR.status == "401") {
+                                    // need to login
+                                    $.when(loginAction()).then(function (accessToken) {
+                                          // login successful, let's retry
+                                          var authorizationHeader = { "Authorization": "Bearer " + accessToken };
+                                          if (optionArgument) {
+                                                optionArgument.headers = authorizationHeader;
+                                          } else {
+                                                urlArgument.headers = authorizationHeader;
+                                          }
+                                          $.when($._ajaxHandler(urlArgument, optionArgument)).then(deferred.resolve, deferred.reject);
+                                    }, function (err) {
+                                          // login failed, pass the error back to the caller
+                                          deferred.reject(arguments);
+                                    });
+                              } else {
                                     deferred.reject(arguments);
-                              });
-                        } else {
-                              deferred.reject(arguments);
-                        }
-                  });
+                              }
+                        });
 
                         return deferred.promise();
                   }
